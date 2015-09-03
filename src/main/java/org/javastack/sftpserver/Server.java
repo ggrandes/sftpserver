d8f0282da6b706e7e183b006c3f16b25bea422c0
ggrandes <guillermo.grandes@gmail.com>
2015-09-03 22:23:01 +0200

    update version

--- src/main/java/org/javastack/sftpserver/Server.java
+++ src/main/java/org/javastack/sftpserver/Server.java
@@ -69,7 +69,7 @@ import org.slf4j.LoggerFactory;
  * @author Guillermo Grandes / guillermo.grandes[at]gmail.com
  */
 public class Server implements PasswordAuthenticator, PublickeyAuthenticator {
-	public static final String VERSION = "1.0.6";
+	public static final String VERSION = "1.0.8";
 	public static final String CONFIG_FILE = "/sftpd.properties";
 	public static final String HOSTKEY_FILE_PEM = "keys/hostkey.pem";
 	public static final String HOSTKEY_FILE_SER = "keys/hostkey.ser";
